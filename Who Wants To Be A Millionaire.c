a05f66672fa2a3effd8801aaade5d200b7fb31b8
RJ Xavier <129512127+RJXavier@users.noreply.github.com>
2023-05-26 22:54:43 +0600

    Add files via upload

---
+++ Who Wants To Be A Millionaire.c
@@ -0,0 +1,431 @@
+#include <stdio.h>
+#include <stdlib.h>
+void Quiz()
+{
+    char choice, choice2, choice3, choice4, choice5, choice6, choice7, choice8, choice9, choice10, choice11, choice12, choice13, choice14, choice15;
+
+        printf("\tWelcome to Who Wants To Be A Millionaire\n");
+        getch();
+        {
+        printf("\t1.What is the height of Mount Everest?\n");
+        printf("\tA.8,848m\tB.8,312m\n\tC.9,680m\tD.7,984m\n");
+        scanf(" %c", &choice);
+        switch (choice){
+    case 'A':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $500!\n");
+        break;
+    case 'B':
+    case 'C':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+
+        }
+        }
+if (choice == 'A')
+        {
+        printf("\n\t2.Nephelococcygia is the practice of doing what?\n");
+        printf("\tA.Sleeping with your eyes open.\tB.Breaking glass with your voice\n\tC.Finding shapes in clouds\tD.Swimming in freezing water\n");
+        scanf(" %c", &choice2);
+        switch (choice2){
+    case 'A':
+    case 'B':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'C':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $1000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice2 == 'C')
+        {
+        printf("\n\t3.Which insect shorted out an early supercomputer and inspired the term 'computer bug'?\n");
+        printf("\tA.Cockroach.\tB.Moth\n\tC.Fly\t\tD.Beetle\n");
+        scanf(" %c", &choice3);
+        switch (choice3){
+    case 'A':
+    case 'C':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'B':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $2000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice3 == 'B')
+        {
+        printf("\n\t4.Who did artist Grant Wood use as the model for the farmer in his classic painting 'American Gothic'?\n");
+        printf("\tA.His Dentist.\tB.His Gardener\n\tC.His Landlord\tD.His Butcher\n");
+        scanf(" %c", &choice4);
+        switch (choice4){
+    case 'B':
+    case 'C':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'A':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $3000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice4 == 'A')
+        {
+        printf("\n\t5.Who was revered as the King of Roman Gods?\n");
+        printf("\tA.Saturn.\tB.Mars\n\tC.Zeus\t\tD.Jupiter\n");
+        scanf(" %c", &choice5);
+        switch (choice5){
+    case 'A':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'D':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $5000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+
+if (choice5 == 'D')
+        {
+        printf("\n\t6.Which fast food chain has the most restaurants in America?\n");
+        printf("\tA.Subway.\tB.KFC\n\tC.Wendy's\tD.McDonald's\n");
+        scanf(" %c", &choice6);
+        switch (choice6){
+    case 'D':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'A':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $7000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice6 == 'A')
+        {
+        printf("\n\t7.What is the fear of clowns called?\n");
+        printf("\tA.Coulrophobia.\t\tB.Agoraphobia\n\tC.Claustrophobia\tD.Acrophobia\n");
+        scanf(" %c", &choice7);
+        switch (choice7){
+    case 'D':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'A':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $10,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice7 == 'A')
+        {
+        printf("\n\t8.Which album has sold the most copies worldwide?\n");
+        printf("\tA.Nevermind by Nirvana.\t\t\t\tB.Saturday Night Fever by The Bees Gees\n\tC.Dark Side of the Moon by Pink Floyd\t\tD.Thriller by Michael Jackson\n");
+        scanf(" %c", &choice8);
+        switch (choice8){
+    case 'A':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'D':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $20,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice8 == 'D')
+        {
+        printf("\n\t9.What was the name of the astronaut who joined Neil Armstrong and Buzz Aldrin on the mission for the first-ever moon landing?\n");
+        printf("\tA.Alan Shepard.\tB.Michael Collins\n\tC.John Glen\tD.Stephen Johnson\n");
+        scanf(" %c", &choice9);
+        switch (choice9){
+    case 'A':
+    case 'D':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'B':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $30,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice9 == 'B')
+        {
+        printf("\n\t10.Which city has the highest murder rate in the world?\n");
+        printf("\tA.Acapulco,Mexico.\t\tB.Detroit,Michigan\n\tC.San Pedro Sula,Honduras\tD.Leeds,London\n");
+        scanf(" %c", &choice10);
+        switch (choice10){
+    case 'A':
+    case 'B':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'C':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $50,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice10 == 'C')
+        {
+        printf("\n\t11.Ralph, Jack, Simon, Piggy, and Roger are all characters of which classic novel?\n");
+        printf("\tA.Animal Farm.\tB.Tale of Two Cities\n\tC.Divine Comedy\tD.Lord of the Flies\n");
+        scanf(" %c", &choice11);
+        switch (choice11){
+    case 'A':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'D':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $100,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice11 == 'D')
+        {
+        printf("\n\t12.What is the proper name of the groove between one's nose and top lip?\n");
+        printf("\tA.Cupid's Bow.\tB.Vermilon Border\n\tC.Ethmoid\tD.Philtrum\n");
+        scanf(" %c", &choice12);
+        switch (choice12){
+    case 'A':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'D':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $250,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice12 == 'D')
+        {
+        printf("\n\t13.What was the name of the plane that carried Charles Lindbergh across the Atlantic Ocean in 1927?\n");
+        printf("\tA.Spirit of St.Louis.\tB.Hindenberg\n\tC.Daredevil\t\tD.Red Baron\n");
+        scanf(" %c", &choice13);
+        switch (choice13){
+    case 'D':
+    case 'B':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'A':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $500,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice13 == 'A')
+        {
+        printf("\n\t14.Which one of these is the oldest game engine still used today?\n");
+        printf("\tA.Unreal Engine.\tB.id Tech Engine\n\tC.CryEngine\t\tD.Unity Engine\n");
+        scanf(" %c", &choice14);
+        switch (choice14){
+    case 'A':
+    case 'D':
+    case 'C':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'B':
+        printf("\tCorrect!\n");
+        printf("\tYou Earn $750,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+if (choice14 == 'B')
+        {
+        printf("\n\t15.During which war was the Star Spangled Banner written?\n");
+        printf("\tA.Revolutionary War.\tB.Civil War\n\tC.War of 1812\t\tD.7 Years War\n");
+        scanf(" %c", &choice15);
+        switch (choice15){
+    case 'A':
+    case 'B':
+    case 'D':
+        printf ("\tWrong Answer!\n\tYou Lose!\n");
+        break;
+    case 'C':
+        printf("\tCorrect!\n");
+        printf("\tCongratulations!You Have Finished The Game!\n\tYou Have Earn $1,000,000!\n");
+        break;
+
+        }
+        }
+else
+    exit(0);
+
+
+}
+
+
+
+
+
+void Instructions()
+{
+printf("\t++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ");
+printf("\t+              Welcome to Who Wants To Be A Millionaire ?          +\n ");
+printf("\t+                                                                   +\n");
+printf("\t+  In this game, you'll have to answer a series of multiple choice +\n ");
+printf("\t+  questions correctly to earn money in each level up to 1 million.+\n ");
+printf("\t+                                                                  +\n ");
+printf("\t+   To answer questions, just type in your choice of A B C or D.   +\n ");
+printf("\t+                        Fail and Start Over                       +\n ");
+printf("\t+                            Good Luck!                            +\n ");
+printf("\t+                                                                  +\n ");
+printf("\t++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ");
+}
+void Menu(){
+printf("\t+----------------------------Main Menu-----------------------------+\n ");
+printf("\t[1] Play Game\n");
+printf("\t[2] Instructions\n");
+printf("\t[3] Exit\n");
+
+}
+
+int main()
+{
+ int a;
+ char MenuChoice, SChoice, choice;
+
+
+printf("\t++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ");
+printf("\t+                                                                  +\n ");
+printf("\t+                 Who Wants To Be A Millionaire ?                  +\n ");
+printf("\t+                                                                  +\n ");
+printf("\t+                                                                  +\n ");
+printf("\t+--------------------------Programmed By---------------------------+\n ");
+printf("\t+                                                                  +\n ");
+printf("\t+                          Raj Xavier                              +\n ");
+printf("\t+                         Ankon Debnath                            +\n ");
+printf("\t+                        Tammanah Yeasmin                          +\n ");
+printf("\t+                                                                  +\n ");
+printf("\t++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ");
+
+getch();
+
+Menu();
+
+getch();
+
+printf("\tEnter Your Option:\n");
+scanf(" %d", &MenuChoice);
+
+switch(MenuChoice){
+
+case 1:
+    Quiz();
+
+case 2:
+    Instructions();
+    getch();
+    main();
+case 3:
+     exit(0);
+default :
+    printf("Invalid Choice");
+    break;
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
